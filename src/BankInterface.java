6fc17bff8a8417933e5cb0fa8f6c9f4f69b30c10
K-0x4b <kareem.m.dev@gmail.com>
2025-06-26 18:19:37 +0000

    Added comments to BankInterface.java

--- src/BankInterface.java
+++ src/BankInterface.java
@@ -1,6 +1,25 @@
+/**
+ * Interface defining basic operations for a bank account.
+ * Any class implementing this interface must provide
+ * methods for depositing, withdrawing, and checking balance.
+ */
 public interface BankInterface {
 
+    /**
+     * Deposits a specified amount into the account.
+     * @param amount the amount to deposit (should be positive)
+     */
     void deposit(double amount);
+
+    /**
+     * Withdraws a specified amount from the account.
+     * @param amount the amount to withdraw (should be positive and <= balance)
+     */
     void withdraw(double amount);
+
+    /**
+     * Returns the current balance of the account.
+     * @return the account balance
+     */
     double getBalance();
 }
